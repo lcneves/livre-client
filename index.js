7999010a173a89a3954968db06e22a3e2abc89e0
Lucas Neves <lcneves@gmail.com>
2017-05-01 23:20:07 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,18 @@
+/*
+ * index.js
+ * Copyright 2017 Lucas Neves <lcneves@gmail.com>
+ *
+ * Entry-point for the livre client.
+ *
+ */
+
+'use strict';
+
+module.exports = function (options) {
+
+  // Configuration
+  const client = null; // TODO
+  const ui = require('./ui.js')(options);
+
+};
+
